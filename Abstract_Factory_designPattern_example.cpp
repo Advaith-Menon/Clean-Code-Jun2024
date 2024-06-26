055842efa0b6f7edb91de88c9102dac267819d71
Advaith-Menon <47732369+Advaith-Menon@users.noreply.github.com>
2024-06-26 21:18:32 +0530

    Create Abstract_Factory_designPattern_example.cpp

---
+++ Abstract_Factory_designPattern_example.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+
+#include <string>
+
+#include <map>
+
+using namespace std;
+
+class I
+{
+    public:
+
+    I() = default;
+
+    virtual ~I() = default;
+ 
+    virtual void operation() = 0;
+};
+
+class Impl : public I
+{
+    public:
+
+     void operation() override{ cout<<"Impl Operation"<<endl;}
+ 
+    static I* CreateMethod() { 
+
+        return new Impl();
+
+    }
+
+    static std::string GetFactoryName() { return "IMPL"; }
+    private:
+
+    static bool registered;
+};
+
+using TCreateMethod = I*(*)();
+
+class IFactory{
+
+     public:
+
+     virtual I* Create(const string& name)=0;
+
+};
+
+class Factory:public IFactory
+
+{
+ 
+public:
+
+    Factory(){}
+ 
+    static bool Register(const string name, TCreateMethod funcCreate);
+ 
+     I* Create(const string& name);
+ 
+private:
+
+    static map<string, TCreateMethod> createMethods;
+
+};
+ 
+map<string, TCreateMethod> Factory::createMethods;
+ 
+bool Factory::Register(const string name, TCreateMethod funcCreate){
+
+         if (auto it = Factory::createMethods.find(name); it == Factory::createMethods.end())
+
+    { 
+
+        Factory::createMethods[name] = funcCreate;
+
+        return true;
+
+    }
+
+    return false;
+
+    }
+
+I* Factory::Create(const string& name){
+
+        if (auto it = Factory::createMethods.find(name); it != Factory::createMethods.end()){ 
+
+        return it->second(); // call the createFunc
+
+        }
+ 
+    return nullptr;
+
+    }
+
+bool Impl::registered =
+
+  Factory::Register(Impl::GetFactoryName(),   
+
+                                     &Impl::CreateMethod);
+
+int main() {
+
+    // Write C++ code here
+
+    std::cout << "Try programiz.pro";
+
+Factory factObj;
+
+IFactory* facPtr=&factObj;
+
+I* ptr=facPtr->Create("IMPL");
+
+ptr->operation();
+
+    return 0;
+
+}
